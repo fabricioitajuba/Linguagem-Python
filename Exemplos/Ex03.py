abe7abd37284ebafba6b2ff96f3ee71091ef6430
Fabricio de Lima Ribeiro <fabricio.engenheiro@gmail.com>
2020-11-06 14:59:12 -0300

    update

--- Exemplos/Ex03.py
+++ Exemplos/Ex03.py
@@ -1,2 +1,10 @@
-for n in range(1, 10, 1):
-    print(n)
+a = 4
+
+if (a == 1) :
+    print("a é igual a 1")
+elif (a == 2) :
+    print("a é igual a 2")
+elif (a == 3) :
+    print("a é igual a 3")
+else :
+    print("a não é igual a nenhuma registrada")
---
+++ Exemplos/Ex04.py
@@ -0,0 +1,3 @@
+#imprime de 1 a 9
+for n in range(1, 10, 1):
+    print(n)
