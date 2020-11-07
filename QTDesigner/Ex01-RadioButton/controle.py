628949cb50646aefcd7c51c210d5ca4da650286b
Fabricio de Lima Ribeiro <fabricio.engenheiro@gmail.com>
2020-11-06 21:12:21 -0300

    update

--- QTDesigner/Ex01-RadioButton/controle.py
+++ QTDesigner/Ex01-RadioButton/controle.py
@@ -1,7 +1,19 @@
 from PyQt5 import uic,QtWidgets
 
 def funcao_principal():
-    print("teste")
+    #print("teste")
+    if formulario.radioButton.isChecked():
+        opcao = "Opção 1 selecionada"
+    elif formulario.radioButton_2.isChecked():
+        opcao = "Opção 2 selecionada"
+    elif formulario.radioButton_3.isChecked():
+        opcao = "Opção 3 selecionada"
+    elif formulario.radioButton_4.isChecked():
+        opcao = "Opção 4 selecionada"
+    else:
+        opcao = ""
+
+    formulario.label_2.setText(opcao)
 
 app=QtWidgets.QApplication([])
 formulario=uic.loadUi("janela.ui")
