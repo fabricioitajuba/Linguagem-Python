712f070cc806d049726433c447b9389f1da5c302
Fabricio de Lima Ribeiro <fabricio.engenheiro@gmail.com>
2020-11-08 13:45:55 -0300

    update

---
+++ Tkinter/Teste-08.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Fabricio de Lima Ribeiro
+#08/11/2020
+#Relógio digital
+#para executar: $ python Teste-08.py
+
+import Tkinter as tk
+import time as tm
+
+def display_time():
+	current_time = tm.strftime('%I:%M:%S:%p')	#12h
+	#current_time = tm.strftime('%H:%M:%S')	#24h
+	clock_label['text'] = current_time
+	my_window.after(1000, display_time)
+
+my_window = tk.Tk()
+my_window.title('Digital Clock')
+clock_label = tk.Label(my_window, font='ariel 80', bg='black', fg='red')
+clock_label.grid(row=0, column=0)
+display_time()
+my_window.mainloop()
