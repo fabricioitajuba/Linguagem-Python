cb3a9e68ee361c1a41c715fcca16a5f90a0955ac
Fabricio Ribeiro <fabricio.engenheiro@gmail.com>
2020-11-16 14:59:46 -0300

    update

---
+++ Exemplos/Ex20.py
@@ -0,0 +1,15 @@
+# Eng. Fabricio de Lima Ribeiro
+# 16/12/2020
+#
+#Este programa permite a entrada de dados no inicio
+#da execussao do programa.
+#
+#Ex:
+#python Ex20.py teste
+#python Ex20.py "Hello World!"
+
+import sys
+
+e = sys.argv[1]
+
+print(e)
---
+++ Exemplos/Ex21.py
@@ -0,0 +1,15 @@
+# Eng. Fabricio de Lima Ribeiro
+# 16/12/2020
+#
+#Este programa faz a soma entre 2 numeros
+#
+#Ex:
+#python Ex21.py 2 3
+
+import sys
+
+a = sys.argv[1]
+b = sys.argv[2]
+c = int(a) + int(b)
+
+print('O valor de A+B=', c)
