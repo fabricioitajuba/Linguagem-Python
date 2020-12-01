bc296067bcaaad6b5f47452949d3176f583c87c2
Fabricio Ribeiro <fabricio.engenheiro@gmail.com>
2020-12-01 10:37:21 -0300

    update

---
+++ Classes/main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+#Fabricio de Lima Ribeiro
+#01/12/2020
+#Exemplo de uso de classes
+#para executar: $ python main.py
+
+from pessoa_fisica import PessoaFisica
+from pessoa_juridica import PessoaJuridica
+
+a = PessoaFisica('111.222.333-4', nome='Alfredo', idade=22)
+
+print(a.getCPF())
+print(a.getNome())
+print(a.getIdade())
+
+b = PessoaJuridica('64.614.527/0001-99', nome='Empresa X', idade=22)
+
+print(b.getCNPJ())
+print(b.getNome())
+print(b.getIdade())
