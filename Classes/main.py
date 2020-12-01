0c2e9ca976866e058770ef7def12edb8e8f2ea25
Fabricio Ribeiro <fabricio.engenheiro@gmail.com>
2020-12-01 10:40:57 -0300

    update

--- Classes/main.py
+++ Classes/main.py
@@ -19,3 +19,9 @@ b = PessoaJuridica('64.614.527/0001-99', nome='Empresa X', idade=22)
 print(b.getCNPJ())
 print(b.getNome())
 print(b.getIdade())
+
+b.setIdade(40)
+
+print(b.getCNPJ())
+print(b.getNome())
+print(b.getIdade())
