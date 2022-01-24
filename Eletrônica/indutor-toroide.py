5feec957770265afee8d134c83bc9886c7f43b83
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2022-01-24 16:13:12 -0300

    Add files via upload

--- Eletrônica/indutor-toroide.py
+++ Eletrônica/indutor-toroide.py
@@ -1,40 +1,53 @@
 # -*- coding: utf-8 -*-
 #Fabricio de Lima Ribeiro
-#18/12/2020
-#Cálculo de indutor com núcleo toroidal
-#para executar: $ python3 indutor-hf.py
+#16/06/2021
+#Cálculo do indutor EE
+#para executar: $ python indutor-EE.py
 
+import os
 import math
 
+#Limpa a tela do console
+os.system('cls')
 
-print("Entre com a indutância desejada em [uH]:")
-L = float(input())
-L /=10**6
+#Entrada
+print("Entre com a indutância [uH]:")
+L=float(input())
+L /= math.pow(10,6)
 
-print("Entre com o Al do nucleo escolhido [nH/n²]:")
-Al = int(input())
-Al /= 10**9
+print("Entre com o AL [nH/N²]:")
+AL=float(input())
+AL /= math.pow(10,9)
+
+print("Entre com a densidade máxima do fluxo magnético [T]:")
+Bmax=float(input())
 
 print("Entre com a corrente de pico [A]:")
-I = float(input())
+Ipk=float(input())
 
-print("Entre com a área esfetiva [cm²]:")
-Ae = float(input())
-Ae /= 10**4
+print("Entre com a área efetiva do núcleo [cm²]:")
+Ae=float(input())
+Ae /= math.pow(10,4)
 
-N = math.sqrt(L/Al)
+#Número de espiras
+N=round(math.sqrt(L/AL), 0)
 
-#Cálculo do B da primeira maneira
-F = N*I #[AE]
-R = 1/Al #[N²/H]
-phi = F/R #[Wb]
-B = phi/Ae #[Tesla]
+F=N*Ipk
+R=1/AL
+phi=F/R
+B=round(phi/Ae, 3)
 
-#Cálculo do B da segunda maneira
-B2 = (L*I)/(Ae*N) #[Tesla]
+#Resultados
+print('\n --- Resultados: ')
+print('Número de espiras: ' + str(N) + " [espiras]")
+print('Força magnetomotriz F= ' + str(F) + " [AE]")
+print('Relutância do núcleo R= ' + str(R) + " [E²/H]")
+print('Fluxo magnético do núcleo phi= ' + str(phi) + " [Wb]")
+print('Campo induzido no núcleo B= ' + str(B) + " [T]")
 
-print(' ')
-print('Numero de espiras do indutor = ' + str(round(N,0)) + " espiras")
-print('B1= '+str(round(B,2))+' [T]')
-print('B2= '+str(round(B2,2))+' [T]')
-print(' ')
+if B <= Bmax:
+    print('O indutor não irá saturar! B <= Bmax ')
+else:
+        print('O indutor irá saturar! B > Bmax ')
+
+print('---')
